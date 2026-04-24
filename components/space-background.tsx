642a0ce4c287a45a4c830a56042b63f2ac0e7065
coffee70 <33002926+coffee70@users.noreply.github.com>
2026-04-24 16:15:00 -0400

    Add sun bloom and richer horizon lighting

--- components/space-background.tsx
+++ components/space-background.tsx
@@ -2,6 +2,7 @@
 
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Stars } from "@react-three/drei";
+import { AdditiveBlending } from "three";
 
 function CameraDrift() {
   useFrame((state) => {
@@ -14,13 +15,61 @@ function CameraDrift() {
   return null;
 }
 
+function SunBloom() {
+  return (
+    <group position={[0, -2.42, -1.2]}>
+      <pointLight color="#ffffff" intensity={18} distance={8} decay={2} />
+
+      <mesh position={[0, 0, 0]}>
+        <sphereGeometry args={[0.05, 32, 32]} />
+        <meshBasicMaterial color="#ffffff" transparent opacity={0.95} />
+      </mesh>
+
+      <mesh position={[0, 0.02, 0]}>
+        <circleGeometry args={[0.9, 96]} />
+        <meshBasicMaterial
+          color="#ffffff"
+          transparent
+          opacity={0.08}
+          blending={AdditiveBlending}
+          depthWrite={false}
+        />
+      </mesh>
+
+      <mesh position={[0, 0.03, 0]}>
+        <circleGeometry args={[1.8, 96]} />
+        <meshBasicMaterial
+          color="#8fcfff"
+          transparent
+          opacity={0.045}
+          blending={AdditiveBlending}
+          depthWrite={false}
+        />
+      </mesh>
+
+      <mesh position={[0, 0.04, 0]}>
+        <circleGeometry args={[3.2, 128]} />
+        <meshBasicMaterial
+          color="#5aaeff"
+          transparent
+          opacity={0.018}
+          blending={AdditiveBlending}
+          depthWrite={false}
+        />
+      </mesh>
+    </group>
+  );
+}
+
 export default function SpaceBackground() {
   return (
     <Canvas camera={{ position: [0, 0.8, 5], fov: 45 }}>
       <color attach="background" args={["#000000"]} />
 
-      <ambientLight intensity={0.05} />
-      <directionalLight position={[0, -2, -4]} intensity={4} />
+      <ambientLight intensity={0.035} />
+      <directionalLight position={[0, -2, -4]} intensity={5} color="#d7eeff" />
+
+      <SunBloom />
 
       {/* Earth */}
       <mesh position={[0, -3.2, 0]} scale={[5, 5, 5]}>
@@ -31,17 +80,17 @@ export default function SpaceBackground() {
       {/* layered glow */}
       <mesh position={[0, -2.2, 0]} rotation={[Math.PI / 2, 0, 0]} scale={[4.8, 0.06, 1]}>
         <torusGeometry args={[1, 0.01, 16, 256]} />
-        <meshBasicMaterial color="#ffffff" transparent opacity={0.9} />
+        <meshBasicMaterial color="#ffffff" transparent opacity={0.95} blending={AdditiveBlending} />
       </mesh>
 
       <mesh position={[0, -2.2, 0]} rotation={[Math.PI / 2, 0, 0]} scale={[5.2, 0.12, 1]}>
         <torusGeometry args={[1, 0.02, 16, 256]} />
-        <meshBasicMaterial color="#a9d8ff" transparent opacity={0.3} />
+        <meshBasicMaterial color="#a9d8ff" transparent opacity={0.38} blending={AdditiveBlending} />
       </mesh>
 
       <mesh position={[0, -2.2, 0]} rotation={[Math.PI / 2, 0, 0]} scale={[6, 0.2, 1]}>
         <torusGeometry args={[1, 0.04, 16, 256]} />
-        <meshBasicMaterial color="#a9d8ff" transparent opacity={0.1} />
+        <meshBasicMaterial color="#6dbbff" transparent opacity={0.14} blending={AdditiveBlending} />
       </mesh>
 
       {/* stars */}
